e732d5368f557dab1b9b0feb36752dd7db715fc9
Jean Venter <jventer1@my.bcit.ca>
2025-10-23 14:57:07 -0700

    added topbar file

---
+++ shadcn/src/app/CallRecordingAndAnalysis/page.tsx
@@ -0,0 +1,14 @@
+// CallRecordingAndAnalysis
+
+import { CallRecordingAndAnalysisTopBar } from "@/components/ui/CallRecordingAndAnalysisTopBar";
+import React from "react";
+
+export default function CallRecordingAndAnalysis() {
+  return (
+    <section className="px-6">
+      <div className="mx-auto max-w-7xl space-y-6">
+        <CallRecordingAndAnalysisTopBar />
+      </div>
+    </section>
+  );
+}
---
+++ shadcn/src/components/ui/CallRecordingAndAnalysisTopBar.tsx
@@ -0,0 +1,7 @@
+export function CallRecordingAndAnalysisTopBar() {
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    );
+}
