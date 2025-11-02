46e6883f908d21091e3a87869ed495f06a8515ee
Cole <cole.goernert@gmail.com>
2025-11-01 18:09:14 -0700

    MockStanderdizationAndComponentFixes
    made mocks for all pipeline components and today's focus components made adjustments and fixes to pipeline cards component

---
+++ shadcn/src/data/mockAchievments.tsx
@@ -0,0 +1,8 @@
+import { MockAchievementItem } from "@/types/types";
+export const mockAchievements: MockAchievementItem[] = [
+  { id: "doc-warrior", current: 8, goal: 10 },
+  { id: "quality-master", current: 4, goal: 5 },
+  { id: "speed-demon", current: 9, goal: 10 },
+];
+
+export default mockAchievements;
